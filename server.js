2604f8a8ea5df1b6e9a89e28c793642a3dcaebd3
shu20031026 <shu20031026@gmail.com>
2022-03-16 12:24:35 +0900

    add: LineBotのロジック

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 const env = require('dotenv').config();
 const express = require('express');
 const line = require('@line/bot-sdk');
+const axios = require('axios');
+const res = require('express/lib/response');
 const PORT = process.env.PORT || 3000;
-
-console.log(env.parsed)
 
 const config = {
   channelSecret: env.parsed.CHANNEL_SECRET,
@@ -15,7 +15,8 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello LINE BOT!(GET)')); 
 app.post('/webhook', line.middleware(config), (req, res) => {
-  console.log(req.body.events);
+  // console.log(req.body)
+  // console.log(req.body.events);
 
   Promise
     .all(req.body.events.map(handleEvent))
@@ -24,15 +25,68 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 
 const client = new line.Client(config);
 
-const handleEvent = async(event) => {
+async function  handleEvent(event){
   if (event.type !== 'message' || event.message.type !== 'text') {
     return Promise.resolve(null);
   }
 
-  return client.replyMessage(event.replyToken, {
+  if(event.message.text === 'ユーザー登録') {
+    let replyText = '';
+    replyText = '登録中。ちょっと待ってね'; //"ちょっと待ってね"ってメッセージだけ先に処理
+    await client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: replyText
+    });
+    
+    const profile = await client.getProfile(event.source.userId);
+
+    const UserId = event.source.userId
+    const USER_POST_URL = `http://localhost:8080/user/${UserId}`
+    await axios.post(USER_POST_URL,{
+      userName: profile.displayName, 
+      introduction: profile.statusMessage, 
+      twitter: "", 
+      github: "", 
+      image: profile.pictureUrl})
+    return client.pushMessage(event.source.userId, {
+        type: 'text',
+        text: '登録しました。',
+    });
+  }
+
+  const ISBN_BASE = "https://api.openbd.jp/v1/get?isbn="
+  const ISBN_QUERY = event.message.text
+  const isbnRes = await axios.get(ISBN_BASE+ISBN_QUERY)
+    .then(res =>{
+      return res.data
+    }).catch(err => {
+      return [null]
+    })
+    
+  if(isbnRes !== undefined && isbnRes !== null && isbnRes[0] !== null){
+    let replyText = '';
+    replyText = '投稿中。ちょっと待ってね'; //"ちょっと待ってね"ってメッセージだけ先に処理
+    await client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: replyText
+    });
+    const bookName = isbnRes[0].summary.title;
+    const BOOK_POST_URL = "http://localhost:8080/books/";
+    const userName = event.source.userId
+    await axios.post(BOOK_POST_URL+userName,{
+        "isbn": ISBN_QUERY,
+        "Article": "from LINE",
+        "lend": false
+    })
+    return client.pushMessage(event.source.userId, {
+        type: 'text',
+        text: `${bookName}を投稿しました。`,
+    });
+  }
+  return client.pushMessage(event.source.userId, {
     type: 'text',
-    text: event.message.text //実際に返信の言葉を入れる箇所
-  });
+    text: "ユーザー登録 : アカウントの登録するよー isbnコードを入力 : 所持している本の登録を行います",
+});
 }
 
 app.listen(PORT);
